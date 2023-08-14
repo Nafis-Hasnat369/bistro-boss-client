98dc79fef0dcb75ae5c7b201cf8d518c933c85d0
Nafis-Hasnat369 <nafis7823@gmail.com>
2023-08-14 19:00:55 +1000

    order menu added

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,7 +1,12 @@
+import { Helmet } from 'react-helmet-async';
+import orderCover from '../../../assets/shop/order.jpg';
+import Cover from '../../Shared/Cover/Cover';
+
 const Order = () => {
     return (
         <div>
-            
+            <Helmet><title>Bistro Boss | Orders</title></Helmet>
+            <Cover img={orderCover} title="Order Food" />
         </div>
     );
 };
